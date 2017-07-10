5e6a06bdc673588001230b0d83c326bc0ab213ba
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2017-07-10 17:44:07 +0530

    Add: Delete/Edit API

--- app/routes/urls.js
+++ app/routes/urls.js
@@ -9,55 +9,90 @@ module.exports = function(apiRoutes) {
 	// apiRoutes.use(function(req, res, next) {
 	// });
 
-	// // Create endpoint /api/beers for Get
+	// // Create endpoint /api/urls for Get
 	apiRoutes.get('/urls', /*authController.isAuthenticated,*/ function(req, res) {
-		// Use the Beer model to find all beer
-		URL.find(function(err, beers) {
+		// Use the url model to find all url
+		URL.find(function(err, urls) {
 			if (err)
 				res.send(err);
 
-			res.json(beers);
+			res.json(urls);
 		});
 	});
 
-	// Create endpoint /api/beer for POSTS
+	// Create endpoint /api/urlScrapper for POSTS
 	apiRoutes.post('/urlScrapper', function(req, res) {
-		// Set the beer properties that came from the POST data
-		 console.log("here",  "req.body.url",  req.body.url)
+		// Set the url properties that came from the POST data
+		// console.log("here", "req.body.url", req.body.url)
 		metascrapper
-		.scrapeUrl(req.body.url)
-		.then((metadata) => {
-			console.log(metadata)
-			var url = new URL({
-				"name": req.body.url,
-				"metadata": metadata
-			});
+			.scrapeUrl(req.body.url)
+			.then((metadata) => {
+				console.log(metadata)
+				var url = new URL({
+					"name": req.body.url,
+					"metadata": metadata
+				});
 
-			// Save the beer and check for errors
-			url.save(function(err) {
-				if (err)
-					res.send(err);
+				// Save the url and check for errors
+				url.save(function(err, data) {
+					if (err)
+						res.send(err);
 
-				res.json(metadata);
-			});
-			// res.json(metadata)
-		})
-		// var beer = new Beer({
-		// 	"name": req.body.name,
-		// 	"type": req.body.type,
-		// 	"quantity": req.body.quantity
-		// });
+					res.json(data);
+				});
+			})
+	});
 
-		// // Save the beer and check for errors
-		// beer.save(function(err) {
-		// 	if (err)
-		// 		res.send(err);
+	// Create endpoint /api/urlScrapper for PUTS
+	apiRoutes.put('/urlScrapper', function(req, res) {
+		// Set the url properties that came from the POST data
+		console.log("put here", "req.body.url", req.body)
+		metascrapper
+			.scrapeUrl(req.body.url)
+			.then((metadata) => {
+				console.log(metadata)
+				var url = new URL({
+					"name": req.body.url,
+					"metadata": metadata
+				});
 
-		// 	res.json({
-		// 		message: 'Beer added to the locker!',
-		// 		data: beer
-		// 	});
-		// });
+				// Save the url and check for errors
+				URL.update({
+					_id: req.body.id
+				},{
+					"$set": {
+						metadata: metadata
+					}
+				}, function(err, data) {
+					if (err)
+						res.send(err);
+
+					URL.findOne({
+						_id: req.body.id
+					}, function(err, url) {
+						if (err)
+							res.send(err);
+
+						res.json(url);
+					});
+
+				});
+			})
+	});
+
+	// Create endpoint /api/urlScrapper/:id for DELETE
+	apiRoutes.delete('/urlScrapper/:id', function(req, res) {
+		// Set the url properties that came from the POST data
+		console.log("here", "req.body.url", req.params)
+		URL.remove({
+			_id: req.params.id
+		}, function(err) {
+			if (err)
+				res.send(err);
+
+			res.send(true);
+		});
+
 	});
 
 	return apiRoutes;
